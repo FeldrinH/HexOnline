0302a7b581598f3d3a866412e29ef07b5699ef48
Olaf Seisler <olaf.seisler@gmail.com>
2020-04-21 11:26:42 -0700

    Add a incomplete Movement logic script. Add print statements to help find coordinates.

--- Scripts/HexTile.gd
+++ Scripts/HexTile.gd
@@ -1,8 +1,14 @@
 extends Area2D
 
+var coordinate : Vector2
+
 func mouse_entered():
 	self.modulate = Color(255, 0, 0)
 
 
 func mouse_exited():
 	self.modulate = Color(255, 255, 255)
+
+func mouse_pressed(Node, event, shape):
+	if event is InputEventMouseButton && event.pressed:
+		print(coordinate)
