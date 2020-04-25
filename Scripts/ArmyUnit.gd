af82fc7e4c7995b3f8903f546f0d26b12c5e3aa6
FeldrinH <fhandswitch@gmail.com>
2020-04-26 02:24:33 +0300

    Improve movement code and animation

--- Scripts/ArmyUnit.gd
+++ Scripts/ArmyUnit.gd
@@ -2,6 +2,9 @@ extends Node2D
 
 const max_power = 100
 
+onready var label = $Label
+onready var movement_tween = $MovementTween
+
 var tile = null
 var power = 0
 
@@ -12,12 +15,24 @@ func init(starting_tile, starting_power):
 func move_to(target_tile):
 	if tile != null:
 		tile.army = null
-	 
-	$Tween.interpolate_property(self, "position", self.position, target_tile.position, 2)
-	$Tween.start()
+	
+	if tile != null:
+		movement_tween.interpolate_property(self, "position", position, target_tile.position, position.distance_to(target_tile.position) / 1000, Tween.TRANS_QUAD, Tween.EASE_IN_OUT)
+		movement_tween.start()
+		yield(movement_tween, "tween_all_completed")
+	else:
+		position = target_tile.position
 	
 	tile = target_tile
-	tile.army = self
+	if tile.army != null:
+		tile.army.merge_with(self)
+	else:
+		tile.army = self
+
+func merge_with(other_army):
+	set_power(power + other_army.power)
+	other_army.queue_free()
 
 func set_power(new_power):
 	power = new_power
+	label.text = str(power)
