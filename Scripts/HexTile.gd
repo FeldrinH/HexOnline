67fdd1aa4700945b8086e6cdc3544e815608e577
FeldrinH <fhandswitch@gmail.com>
2020-04-25 23:28:45 +0300

    Add correct tile coordinates

--- Scripts/HexTile.gd
+++ Scripts/HexTile.gd
@@ -1,14 +1,32 @@
 extends Area2D
 
+onready var tile_manager = $".."
+
 var coordinate : Vector2
+var active : bool = false
 
 func mouse_entered():
-	self.modulate = Color(255, 0, 0)
-
+	set_active(true)
 
 func mouse_exited():
-	self.modulate = Color(255, 255, 255)
+	set_active(false)
 
-func mouse_pressed(Node, event, shape):
-	if event is InputEventMouseButton && event.pressed:
+func set_active(new_active):
+	if new_active:
+		if tile_manager.active_tile != null:
+			tile_manager.active_tile.set_active(false)
+		
+		tile_manager.active_tile = self
+		
+		self.modulate = Color(255,0,0)
+	else:
+		if tile_manager.active_tile == self:
+			tile_manager.active_tile = null
+		
+		self.modulate = Color(255,255,255)
+	
+	active = new_active
+
+func mouse_pressed(Node, event : InputEvent, shape):
+	if event.is_action_pressed("ui_mouse_left"):
 		print(coordinate)
--- Scripts/MapGridManager.gd
+++ Scripts/MapGridManager.gd
@@ -5,13 +5,16 @@ const tile_scene = preload("res://HexTile.tscn")
 onready var tilemap : TileMap = $"../TileMap"
 
 const tile_dict = {}
+var active_tile = null
 
 func _ready():
+	print(posmod(-3, 2))
+	
 	for coordinate in tilemap.get_used_cells():
 		var tile_instance = tile_scene.instance()
 		tile_instance.set_position(tilemap.map_to_world(coordinate))
 
-		coordinate.x *= 2
+		coordinate.x = coordinate.x * 2 + posmod(coordinate.y, 2)
 		tile_dict[coordinate] = tile_instance
 		tile_instance.coordinate = coordinate
 		tile_instance.set_name(str(coordinate))
@@ -19,3 +22,6 @@ func _ready():
 		add_child(tile_instance)
 
 	tilemap.queue_free()
+
+func get_tile(coord):
+	return tile_dict[coord]
