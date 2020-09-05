f987bd644c6f5559e5285b41da1be774c74c610b
Olaf Seisler <olaf.seisler@gmail.com>
2020-09-05 22:20:47 +0300

    Add new city tiles. Alter a forest tile. Implementation needed.

--- Scripts/City.gd
+++ Scripts/City.gd
@@ -5,7 +5,7 @@ onready var port_icon = $PortIcon
 onready var city_icon = $CityIcon
 onready var city_sprite = $CitySprite
 
-const city_tiles = [preload("res://Sprites/city_tile_1.png"), preload("res://Sprites/city_tile_2.png")]
+const city_tiles = [preload("res://Sprites/city_tile_1.png"), preload("res://Sprites/city_tile_2.png"), preload("res://Sprites/city_tile_3.png"), preload("res://Sprites/city_tile_2.png"), preload("res://Sprites/city_tile_3.png"), preload("res://Sprites/city_tile_4.png"), preload("res://Sprites/city_tile_5.png"), preload("res://Sprites/city_tile_2.png"), preload("res://Sprites/city_tile_3.png"), preload("res://Sprites/city_tile_6.png")]
 
 var city_name : String
 
