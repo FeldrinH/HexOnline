1d7a0a84b46341feab78221dc39e27b40414b244
Olaf Seisler <olaf.seisler@gmail.com>
2021-08-14 19:06:01 +0300

    Add a basic function to assign weights to tiles.

--- Scripts/AIManager.gd
+++ Scripts/AIManager.gd
@@ -33,7 +33,7 @@ func _on_current_player_changed(new_player: Node):
 		
 		var player_units = world.get_player_units(new_player)
 		for unit in player_units:
-			var shortest_path = find_shortest_path(unit.tile, enemy_capital_tile, unit)
+			var shortest_path = find_shortest_path(unit.tile, enemy_capital_tile, new_player)
 			print(shortest_path)
 			for tile in shortest_path:
 				tile.debug_label.text = str(unit.name)
@@ -44,14 +44,23 @@ func _on_current_player_changed(new_player: Node):
 		
 		world.game.rpc("skip_turn", new_player.id)
 			
-func find_shortest_path(start, target, army) -> Array:
-	astar.load_map(world, army)
+func find_shortest_path(start, target, player) -> Array:
+	astar.load_map(world, player)
 	var tile_path = []
 	var path = astar.get_id_path(start.id, target.id)
 	for i in path:
 		tile_path.append(world.get_tile_by_id(i))
 	return tile_path
-		
+
+func assign_weight(unit_tile, player):
+	for tile in world.find_travelable(unit_tile, player, 2):
+		if tile.army and tile.player != player:
+			return Util.distance_between(unit_tile.coord, tile.coord)/2 * 0.5
+		elif tile.city:
+			return 0.5
+	return 1
+
+
 #func find_shortest_path_util(current_tile, target, army, path) -> Array:
 #	var next_dist = world.distance_between(current_tile, target)
 #	var next_tile = current_tile
--- Scripts/HexAStar.gd
+++ Scripts/HexAStar.gd
@@ -1,13 +1,14 @@
 extends AStar2D
 
-func load_map(world, unit):
+func load_map(world, player):
 	clear()
 	var tiles = world.get_all_tiles()
 	for tile in tiles:
 		var weight = 1 if tile.city else 2
-		add_point(tile.id, tile.coord, weight)	
+		add_point(tile.id, tile.coord, weight)
 	for tile in tiles:
-		for travelable in world.find_travelable(tile, unit, 2):
+		
+		for travelable in world.find_travelable(tile, player, 2):
 			 connect_points(tile.id, travelable.id)
 
 func _compute_cost(u, v):
@@ -15,4 +16,3 @@ func _compute_cost(u, v):
 
 func _estimate_cost(u : int, v : int):
 	return Util.distance_between(get_point_position(u), get_point_position(v)) / 2
-
